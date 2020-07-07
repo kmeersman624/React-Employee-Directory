f99a8792e5948a24764f6450e4de10987fdb4041
kmeersman <katie4682@yahoo.com>
2020-07-07 10:35:25 -0600

    Updates

---
+++ precache-manifest.c9ebf9b79d50db2a5e668614e7b5b6a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cef6679f5a0e897932650b7942b4ceb",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "f8c1729529a4633279e7",
+    "url": "/React-Employee-Directory/static/css/main.708095b8.chunk.css"
+  },
+  {
+    "revision": "9fc1ea8060753de4f13b",
+    "url": "/React-Employee-Directory/static/js/2.c37109b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.c37109b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c1729529a4633279e7",
+    "url": "/React-Employee-Directory/static/js/main.8ddab164.chunk.js"
+  },
+  {
+    "revision": "7a3b04eb39eeeb27bc59",
+    "url": "/React-Employee-Directory/static/js/runtime-main.a0389cb5.js"
+  }
+]);
